c242b5aef780d0dc8da90aa64bfb287d1a619da0
9node <rayyan@theninthnode.com>
2024-10-07 08:36:48 +0530

    added .config

---
+++ .config/pragtical/session.lua
@@ -0,0 +1,16 @@
+return {
+  ["previous_find"] = {},
+  ["previous_replace"] = {},
+  ["recents"] = {
+    [2] = "/home/nixos/rails",
+    [1] = "/home/nixos/rails/blog"
+  },
+  ["window"] = {
+    [1] = 1908,
+    [2] = 987,
+    [3] = 6,
+    [4] = 27,
+    ["n"] = 4
+  },
+  ["window_mode"] = "maximized"
+}
